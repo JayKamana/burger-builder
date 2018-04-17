e499c69c8fee0164fc037137d6e6a701d33c54be
James Kamana <jaykamana@gmail.com>
2018-04-17 13:24:02 +0300

    Add current price display

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -41,13 +41,45 @@ class BurgerBuilder extends Component {
     });
   };
 
-  removeIngredientHandler = type => {};
+  removeIngredientHandler = type => {
+    const oldCount = this.state.ingredients[type];
+    if (oldCount <= 0) {
+      return;
+    }
+    const updatedCount = oldCount - 1;
+    const updatedIngredients = {
+      ...this.state.ingredients
+    };
+    updatedIngredients[type] = updatedCount;
+
+    const priceDeduction = INGREDIENT_PRICES[type];
+
+    const oldPrice = this.state.totalPrice;
+
+    const updatedPrice = oldPrice - priceDeduction;
+
+    this.setState({
+      ingredients: updatedIngredients,
+      totalPrice: updatedPrice
+    });
+  };
 
   render() {
+    const disablesInfo = {
+      ...this.state.ingredients
+    };
+    for (let key in disablesInfo) {
+      disablesInfo[key] = disablesInfo[key] <= 0;
+    }
     return (
       <Auxiliary>
         <Burger ingredients={this.state.ingredients} />
-        <BuildControls ingredientAdded={this.addIngredientHandler} />
+        <BuildControls
+          ingredientAdded={this.addIngredientHandler}
+          ingredientRemoved={this.removeIngredientHandler}
+          disabled={disablesInfo}
+          price={this.state.totalPrice}
+        />
       </Auxiliary>
     );
   }
