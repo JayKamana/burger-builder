ecee5e2b2f76c36191d1789e70dfbb0078e6a77c
James Kamana <jaykamana@gmail.com>
2018-04-19 18:49:57 +0300

    Add Navigation items to toolbar

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,13 @@
 import React from "react";
 import Auxiliary from "../../hoc/Auxiliary";
 import classes from "./Layout.css";
+import Toolbar from "../Navigation/Toolbar/Toolbar";
 
 const Layout = props => (
   <Auxiliary>
-    <div>Toolbar, SideDrawer, Backdrop</div>
+    <div>
+      <Toolbar />
+    </div>
     <main className={classes.Content}>{props.children}</main>
   </Auxiliary>
 );
