9fbbebfc399782630f9ed8b6fcb9fb448d051d1b
James Kamana <jaykamana@gmail.com>
2018-05-14 22:07:25 +0300

    Combine burger builder and order reducers

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -193,15 +193,17 @@ class ContactData extends Component {
 
 const mapStateToProps = state => {
   return {
-    ings: state.ingredients,
-    price: state.totalPrice,
-    loading: state.loading
+    ings: state.burgerBuilder.ingredients,
+    price: state.burgerBuilder.totalPrice,
+    loading: state.order.loading
   };
 };
 
 const mapDispatchToProps = dispatch => {
   return {
-    onOrderBurger: orderData => dispatch(actions.purchaseBurger())
+    onOrderBurger: orderData => {
+      return dispatch(actions.purchaseBurger(orderData));
+    }
   };
 };
 
